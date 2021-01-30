9f01544998fb2af7b19bb29bd9efe208f2a10e46
i514097 <tianli.gao@sap.com>
2021-01-30 13:02:33 -0500

    initial commit

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {ProductsComponent} from "./products/products.component";
+import {ShoppingCartComponent} from "./shopping-cart/shopping-cart.component";
+import {OrdersComponent} from "./orders/orders.component";
 
 @Component({
   selector: 'app-store',
@@ -6,10 +9,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./store.component.css']
 })
 export class StoreComponent implements OnInit {
+  private collapsed = true;
+  orderFinished = false;
+
+  // @ViewChild('productsC')
+  // productsC: ProductsComponent;
+
+  // @ViewChild('shoppingCartC')
+  // shoppingCartC: ShoppingCartComponent;
+
+  // @ViewChild('ordersC')
+  // ordersC: OrdersComponent;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleCollapsed(): void {
+    this.collapsed = !this.collapsed;
+  }
+
+  isCollapsed(): boolean {
+    return this.collapsed;
+  }
+
+  finishOrder(orderFinished: boolean) {
+      this.orderFinished = orderFinished;
+  }
+
+  reset() {
+      this.orderFinished = false;
+      //this.productsC.reset();
+      //this.shoppingCartC.reset();
+      //this.ordersC.paid = false;
   }
 
 }
