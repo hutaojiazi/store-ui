e4e8eca616f460c140cde4fe853fdac38d1eaa8e
i514097 <tianli.gao@sap.com>
2021-01-30 13:33:26 -0500

    initial commit

--- src/app/store/products/products.component.ts
+++ src/app/store/products/products.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {ProductOrder} from "../models/product-order.model";
+import {StoreService} from "../services/StoreService";
+import {Subscription} from "rxjs/internal/Subscription";
+import {ProductOrders} from "../models/product-orders.model";
+import {Product} from "../models/product.model";
 
 @Component({
   selector: 'app-products',
@@ -7,9 +12,73 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  productOrders: ProductOrder[] = [];
+  products: Product[] = [];
+  selectedProductOrder: ProductOrder;
+  private shoppingCartOrders: ProductOrders;
+  sub: Subscription;
+  productSelected: boolean = false;
 
-  ngOnInit(): void {
+  constructor(private storeService: StoreService) {
+  }
+
+  ngOnInit() {
+      this.productOrders = [];
+      this.loadProducts();
+      this.loadOrders();
+  }
+
+  addToCart(order: ProductOrder) {
+      this.storeService.SelectedProductOrder = order;
+      this.selectedProductOrder = this.storeService.SelectedProductOrder;
+      this.productSelected = true;
+  }
+
+  removeFromCart(productOrder: ProductOrder) {
+      let index = this.getProductIndex(productOrder.product);
+      if (index > -1) {
+          this.shoppingCartOrders.productOrders.splice(
+              this.getProductIndex(productOrder.product), 1);
+      }
+      this.storeService.ProductOrders = this.shoppingCartOrders;
+      this.shoppingCartOrders = this.storeService.ProductOrders;
+      this.productSelected = false;
+  }
+
+  getProductIndex(product: Product): number {
+      return this.storeService.ProductOrders.productOrders.findIndex(
+          value => value.product === product);
+  }
+
+  isProductSelected(product: Product): boolean {
+      return this.getProductIndex(product) > -1;
+  }
+
+  loadProducts() {
+      this.storeService.getAllProducts()
+          .subscribe(
+              (products: any[]) => {
+                  this.products = products;
+                  this.products.forEach(product => {
+                      this.productOrders.push(new ProductOrder(product, 0));
+                  })
+              },
+              (error) => console.log(error)
+          );
+  }
+
+  loadOrders() {
+      this.sub = this.storeService.OrdersChanged.subscribe(() => {
+          this.shoppingCartOrders = this.storeService.ProductOrders;
+      });
+  }
+
+  reset() {
+      this.productOrders = [];
+      this.loadProducts();
+      this.storeService.ProductOrders.productOrders = [];
+      this.loadOrders();
+      this.productSelected = false;
   }
 
 }
--- src/app/store/shopping-cart/shopping-cart.component.ts
+++ src/app/store/shopping-cart/shopping-cart.component.ts
@@ -1,15 +1,78 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import {ProductOrders} from "../models/product-orders.model";
+import {ProductOrder} from "../models/product-order.model";
+import {StoreService} from "../services/StoreService";
+import {Subscription} from "rxjs/internal/Subscription";
 
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
   styleUrls: ['./shopping-cart.component.css']
 })
-export class ShoppingCartComponent implements OnInit {
+export class ShoppingCartComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  orderFinished: boolean;
+  orders: ProductOrders;
+  total: number;
+  sub: Subscription;
 
-  ngOnInit(): void {
+  @Output() onOrderFinished: EventEmitter<boolean>;
+
+  constructor(private storeService: StoreService) {
+      this.total = 0;
+      this.orderFinished = false;
+      this.onOrderFinished = new EventEmitter<boolean>();
+  }
+
+  ngOnInit() {
+      this.orders = new ProductOrders();
+      this.loadCart();
+      this.loadTotal();
+  }
+
+  private calculateTotal(products: ProductOrder[]): number {
+      let sum = 0;
+      products.forEach(value => {
+          sum += (value.product.price * value.quantity);
+      });
+      return sum;
+  }
+
+  ngOnDestroy() {
+      this.sub.unsubscribe();
+  }
+
+  finishOrder() {
+      this.orderFinished = true;
+      this.storeService.Total = this.total;
+      this.onOrderFinished.emit(this.orderFinished);
+  }
+
+  loadTotal() {
+      this.sub = this.storeService.OrdersChanged.subscribe(() => {
+          this.total = this.calculateTotal(this.orders.productOrders);
+      });
+  }
+
+  loadCart() {
+      this.sub = this.storeService.ProductOrderChanged.subscribe(() => {
+          let productOrder = this.storeService.SelectedProductOrder;
+          if (productOrder) {
+              this.orders.productOrders.push(new ProductOrder(
+                  productOrder.product, productOrder.quantity));
+          }
+          this.storeService.ProductOrders = this.orders;
+          this.orders = this.storeService.ProductOrders;
+          this.total = this.calculateTotal(this.orders.productOrders);
+      });
+  }
+
+  reset() {
+      this.orderFinished = false;
+      this.orders = new ProductOrders();
+      this.orders.productOrders = []
+      this.loadTotal();
+      this.total = 0;
   }
 
 }
